b6ddb159dc7dbe85da98902faed802493f19dbf2
moonbyeongmin <qudalsrnt3x@gmail.com>
2024-11-07 15:21:19 +0900

    🥔init: 프로젝트 생성
    프로젝트 초기 생성

---
+++ src/main/java/com/sparta/gamjaquick/GamjaQuickApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.gamjaquick;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GamjaQuickApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GamjaQuickApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sparta/gamjaquick/GamjaQuickApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.gamjaquick;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GamjaQuickApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
