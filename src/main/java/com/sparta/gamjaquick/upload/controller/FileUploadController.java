c36599b48ab966adc931a592c48bad8eaf3e68a5
ykybkoft <126169447+ykybkoft@users.noreply.github.com>
2024-11-18 21:33:37 +0900

    ✨feat : file upload 스웨거 어노테이션 추가 & review 예외 코드 추가

--- src/main/java/com/sparta/gamjaquick/upload/controller/FileUploadController.java
+++ src/main/java/com/sparta/gamjaquick/upload/controller/FileUploadController.java
@@ -4,6 +4,8 @@ import com.sparta.gamjaquick.common.response.ApiResponseDto;
 import com.sparta.gamjaquick.common.response.MessageType;
 import com.sparta.gamjaquick.upload.dto.response.UploadResponseDto;
 import com.sparta.gamjaquick.upload.service.FileUploadService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,12 +17,14 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/uploads")
+@Tag(name = "File Upload", description = "파일 업로드 API")
 @RequiredArgsConstructor
 public class FileUploadController {
 
     private final FileUploadService fileUploadService;
 
     @PostMapping("")
+    @Operation(summary = "S3 사진 업로드", description = "S3에 사진을 업로드 할 때 사용하는 API")
     public ApiResponseDto<?> uploadFiles(@RequestParam(value = "files", required = false) List<MultipartFile> files) {
         List<UploadResponseDto> result = fileUploadService.uploadFiles(files);
         return ApiResponseDto.success(MessageType.CREATE, result);
